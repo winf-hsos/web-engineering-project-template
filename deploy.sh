dc8deaf9db7343c2aa11585fb1bda9a1b14c1b5e
Nicolas Meseth <n.meseth@hs-osnabrueck.de>
2018-04-11 07:39:29 +0000

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Copying files to public..."
+cd /home/ubuntu/workspace/web-engineering-project-template
+rm -r public/*
+cp -f *.html public
+cp -R css public/css
+cp -R js public/js
+cp -R includes public/includes
+cp -R img public/img
+
+echo "Deploying to Firebase..."
+firebase deploy
